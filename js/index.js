66bae8ccae14a0c69cec1e3c027bc2370853b6a4
rapunay <rapunay@yahoo.com>
2016-07-26 11:21:41 +0800

    Add project files
    Initial files for the project

---
+++ js/index.js
@@ -0,0 +1,105 @@
+"use strict"
+
+$(function(){
+	var viewPort = new Project.ViewPort({
+		elem:$("#viewPort"),
+		rows: 5,
+		cols: 5
+	});
+	viewPort.init();
+});
+
+var Project;
+(function(Project){
+	Project.ViewPort = (function(){
+		function ViewPort(options){
+			this.elem;
+			this.rows;
+			this.cols;
+			this.inputField;
+			this.button;
+			this.pond;
+			
+			$.extend(this, options);
+		};
+			
+		$.extend(ViewPort.prototype, {
+			init: function(){
+				_initContent(this);
+				_initEventHandlers(this);
+			}
+		});
+			
+		function _initContent(ViewPort){
+			var $viewPort = ViewPort.elem,
+				$inputField = $viewPort.find("#commandField"),
+				$button = $viewPort.find("#commandBtn");
+			
+			ViewPort.inputField = $inputField;
+			ViewPort.button = $button;
+			
+			ViewPort.pond = new Pond({
+				rows:ViewPort.rows,
+				cols:ViewPort.cols
+			});
+			$viewPort.append(ViewPort.pond.create());
+			
+			ViewPort.inputField.focus();
+		};
+		
+		function _initEventHandlers(ViewPort){
+			ViewPort.button.on("click", {_ViewPort: ViewPort}, function(e){
+				var _ViewPort = e.data._ViewPort;
+				_getAndExecuteCommand(_ViewPort);
+			});
+			
+			ViewPort.inputField.on("keydown", {_ViewPort: ViewPort}, function(e){
+				if(e.which == 13){
+					var _ViewPort = e.data._ViewPort;
+					_getAndExecuteCommand(_ViewPort);
+				}
+			});
+			
+			ViewPort.elem.find("#pond").on("keydown", {_pond: ViewPort.pond}, function(e){
+				var _pond = e.data._pond;
+			
+				if(e.which == 38){
+					_pond.executeCommand("MOVE");
+				}else if(e.which == 37){
+					_pond.executeCommand("LEFT");
+				}else if(e.which == 39){
+					_pond.executeCommand("RIGHT");
+				}else if(e.which == 40){
+					_pond.executeCommand("REPORT");
+				}
+			});
+			
+			ViewPort.elem.find("#pond .col").on("click", {_ViewPort: ViewPort}, function(e){
+				var _ViewPort = e.data._ViewPort,
+					pond = _ViewPort.pond,
+					row = $(e.target).closest(".row").attr("id"),
+					col = $(e.target).attr("id"),
+					direction = "NORTH";
+					
+				if(!pond.turtle){
+					pond.turtle = pond.createTurtle(pond.container.find("#pond"), _ViewPort.rows-1, _ViewPort.cols-1);
+				}else{
+					direction = pond.turtle.direction;
+				}
+					
+				pond.turtle.place(col, row, direction);
+			});
+		};
+		
+		function _getAndExecuteCommand(viewPort){
+			var command = viewPort.inputField.val();
+			
+			if(command != ""){
+				viewPort.pond.executeCommand(command);
+				viewPort.inputField.val("");
+			}
+		};
+		
+		return ViewPort;
+	})();
+})(Project || (Project = {}));
---
+++ js/pond.js
@@ -0,0 +1,199 @@
+"use strict"
+
+var Pond = (function(){
+	function Pond(options){
+		this.container;
+		this.turtle;
+		this.rows;
+		this.cols;
+		
+		$.extend(this, options);
+	};
+	
+	var shortErrors = {
+			execErr: "Unable to execute command!"
+		},
+		longErrors = {
+			tooFewArgs: "There are too few information for the command.",
+			tooManyArgs: "There are too many information for the command.",
+			placeInvalidPos: "Please enter a valid coordinates for PLACE command.",
+			noTurtle: "Please put a turtle in the pond using PLACE before executing another command.",
+			invCommand: "The command is not available in the list of supported commands."
+		};
+	
+	$.extend(Pond.prototype, {
+		create: function(){
+			var $pondContainer = $("<div><div class='errorContainer'><div class='shortErr'></div><div class='longErr'></div></div><div id='pond' tabindex='-1'></div></div>"),
+				pondContent = [];
+			
+			for(var i=(this.rows-1); i>=0; i--){
+				pondContent.push("<div class='row' id='" +i+ "'>");
+				for(var j=0; j<this.cols; j++){
+					pondContent.push("<div class='col' id='" +j+ "'></div>");
+				}
+				pondContent.push("</div>")
+			}
+			$pondContainer.find("#pond").append(pondContent.join(""));
+			
+			this.container = $pondContainer;
+			return $pondContainer;
+		},
+		
+		executeCommand: function(command){
+			var params = command.toUpperCase().split(" "),
+				$errorCont = this.container.find(".errorContainer");
+			
+			if(params[0] == "PLACE"){
+				if(params.length < 2){
+					_displayError($errorCont, shortErrors["execErr"], longErrors["tooFewArgs"]);
+				}else if(params.length > 2){
+					_displayError($errorCont, shortErrors["execErr"], longErrors["tooManyArgs"]);
+				}else{
+					var placePos = params[1].split(","),
+						turtle = this.turtle;
+					
+					if(placePos.length < 3 || placePos.length > 3){
+						_displayError($errorCont, shortErrors["execErr"], longErrors["placeInvalidPos"]);
+					}else{
+						if(!turtle){
+							turtle = this.createTurtle(this.container.find("#pond"), this.rows-1, this.cols-1);
+						}
+						if(turtle.place(placePos[0], placePos[1], placePos[2])){
+							this.turtle = turtle;
+							_hideError($errorCont);
+						}else{
+							_displayError($errorCont, shortErrors["execErr"], longErrors["placeInvalidPos"]);
+						}
+					}
+				}
+			}else{
+				if(!this.turtle){
+					_displayError($errorCont, shortErrors["execErr"], longErrors["noTurtle"]);
+				}else{
+					if(params.length > 1){
+						_displayError($errorCont, shortErrors["execErr"], longErrors["tooManyArgs"]);
+					}else{
+						if(params[0] == "MOVE"){
+							this.turtle.move();
+							_hideError($errorCont);
+						}else if(params[0] == "LEFT"){
+							this.turtle.left();
+							_hideError($errorCont);
+						}else if(params[0] == "RIGHT"){
+							this.turtle.right();
+							_hideError($errorCont);
+						}else if(params[0] == "REPORT"){
+							this.turtle.report();
+							_hideError($errorCont);
+						}else{
+							_displayError($errorCont, shortErrors["execErr"], longErrors["invCommand"]);
+						}
+						
+						
+					}
+				}
+			}
+		},
+		
+		createTurtle: function($pond, maxRow, maxCol){
+			var turtle = new Turtle({
+					pond: $pond,
+					maxRow:maxRow,
+					maxCol:maxCol
+				});
+				
+			return turtle;
+		}
+	});
+	
+	function _displayError($errorCont, shortErr, longErr){
+		$errorCont.addClass("hasError");
+		$errorCont.find(".shortErr").empty().text(shortErr);
+		$errorCont.find(".longErr").empty().text(longErr);
+	};
+	
+	function _hideError($errorCont){
+		$errorCont.removeClass("hasError");
+		$errorCont.find(".shortErr").empty();
+		$errorCont.find(".longErr").empty();
+	};
+	
+	return Pond;
+})();
+
+var Turtle = (function(){
+	function Turtle(options){
+		this.pond = $(document);
+		this.maxCol;
+		this.maxRow;
+		this.col;
+		this.row;
+		this.direction = "";
+		
+		$.extend(this, options)
+	};
+	
+	var directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+	
+	$.extend(Turtle.prototype, {
+		place: function(col, row, direction){
+			if(directions.indexOf(direction) < 0){
+				return 0;
+			}
+			if(isNaN(col) || parseInt(col) > this.maxCol || parseInt(col) < 0){
+				return 0;
+			}
+			if(isNaN(row) || parseInt(row) > this.maxRow || parseInt(row) < 0){
+				return 0;
+			}
+		
+			var $pond = this.pond,
+				pondCell = $pond.find(".row#" +row+ " .col#" +col),
+				turtleCell = $pond.find(".turtleCell");
+		
+			turtleCell.removeClass("turtleCell");
+			turtleCell.removeClass(this.direction.toLowerCase());
+			pondCell.addClass("turtleCell");
+			pondCell.addClass(direction.toLowerCase());
+			
+			this.col = col;
+			this.row = row;
+			this.direction = direction;
+			return 1;
+		},
+		
+		move: function(){
+			var direction = this.direction,
+				col = parseInt(this.col, 10),
+				row = parseInt(this.row, 10);
+			
+			if(direction == "NORTH" && parseInt(row) < this.maxRow){
+					row++;
+			}else if(direction == "EAST" && parseInt(col) < this.maxCol){
+					col++;
+			}else if(direction == "WEST" && parseInt(col) > 0){
+					col--;
+			}else if(direction == "SOUTH" && parseInt(row) > 0){
+					row--;
+			}
+			
+			this.place(col, row, direction);
+		},
+		
+		right: function(){
+			var newDirection = (directions.indexOf(this.direction) + 1) % directions.length;
+			this.place(this.col, this.row, directions[newDirection]);
+		},
+		
+		left: function(){
+			var newDirection = (directions.indexOf(this.direction) + directions.length - 1) % directions.length;
+			this.place(this.col, this.row, directions[newDirection]);
+		},
+		
+		report: function(){
+			console.log(this.col, this.row, this.direction.toUpperCase());
+		}
+	});
+	
+	return Turtle;
+})();
